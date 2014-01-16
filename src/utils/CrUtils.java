c72d2f2ebab49ed294f2d1852ff3c3dadb8016c7
paglima <paulopagl@gmail.com>
2014-01-16 07:58:46 -0200

    fazendo metodo printConteudo receber um crService, fazendo com que a classe CrUtils nao saiba como ler o arquivo

--- src/utils/CrUtils.java
+++ src/utils/CrUtils.java
@@ -1,35 +1,36 @@
 package utils;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.List;
+
+import model.Disciplina;
+import service.CrService;
 
 public class CrUtils {
 
 	private static final int TOTAL_DE_DISCIPLINAS = 49;
 
-	public static void printConteudo(String path) throws FileNotFoundException {
+	public static void printConteudo(CrService crService) throws FileNotFoundException {
 		System.out.format("%13s%14s%14s%14s\n", "NOME","NOTA","CH","SEMESTRE");
 		
-		Scanner scan = new Scanner(new File(path));
+		List<Disciplina> disciplinas = crService.findAll();		
 		int cont = 0, cont2 = 0;
 		
-		while (scan.hasNextLine()) {
-			String nome		  = scan.next();
-			Double nota 	  = scan.nextDouble();
+		for (Disciplina disciplina : disciplinas) {
+			String nome		  = disciplina.getNome();
+			Double nota 	  = disciplina.getNota();
 			boolean ehEstagio = !ehEstagioOuProjeto(nome);
 			if(foiAprovado(nota)){
-				System.out.format("%13s%14s%14s%14s\n", nome, nota.toString(), scan.nextInt(), scan.next());
+				System.out.format("%13s%14s%14s%14s\n", nome, nota.toString(), disciplina.getCargaHoraria(), disciplina.getPeriodo());
 				cont += ehEstagio? 1: 0;
 			}else{
-				System.out.format("%13s%14s%14s%14s\n", nome, nota.toString(), scan.nextInt(), "[REP] "+scan.next());
+				System.out.format("%13s%14s%14s%14s\n", nome, nota.toString(), disciplina.getCargaHoraria(), "[REP] "+disciplina.getPeriodo());
 			}
 			cont2 += ehEstagio? 1: 0;
 		}
-		
 		System.out.format("\n%14s\t\t%s","AP:" , cont+"/"+cont2);
 		System.out.format("\n%14s\t\t%s","Restantes:"  ,( TOTAL_DE_DISCIPLINAS - (cont)));
-		scan.close();
+			
 	}
 
 	private static boolean ehEstagioOuProjeto(String nome) {
